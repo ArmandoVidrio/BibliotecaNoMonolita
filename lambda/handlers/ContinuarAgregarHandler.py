4f2e0412707be42781cf4ae93207cda8ddd6254b
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2025-09-26 05:01:22 +0000

    updated lambda/handlers/ContinuarAgregarHandler.py.

--- lambda/handlers/ContinuarAgregarHandler.py
+++ lambda/handlers/ContinuarAgregarHandler.py
@@ -3,9 +3,9 @@ from datetime import datetime
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
 import ask_sdk_core.utils as ask_utils
 
-from helpers.database import DatabaseManager
+from datasources.DataPersistency import DatabaseManager
 from helpers.utils import generar_id_unico, get_random_phrase
-from helpers.frases import ALGO_MAS, PREGUNTAS_QUE_HACER
+from enums import ALGO_MAS, PREGUNTAS_QUE_HACER
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)

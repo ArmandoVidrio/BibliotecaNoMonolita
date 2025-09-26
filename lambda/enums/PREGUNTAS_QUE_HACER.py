ee65bd2392b5902320bfc7a557d04747e47ed88f
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2025-09-26 04:05:22 +0000

    updated lambda/enums/PREGUNTAS_QUE_HACER.py.

---
+++ lambda/enums/PREGUNTAS_QUE_HACER.py
@@ -0,0 +1,7 @@
+PREGUNTAS_QUE_HACER = [
+    "¿Qué te gustaría hacer hoy?",
+    "¿En qué puedo ayudarte?",
+    "¿Qué necesitas?",
+    "¿Cómo puedo ayudarte con tu biblioteca?",
+    "¿Qué quieres hacer?"
+]

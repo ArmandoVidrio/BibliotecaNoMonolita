13291051da2ca55385f744bdcff3738939119ec3
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2025-09-26 04:04:11 +0000

    updated lambda/enums/SALUDOS.py.

---
+++ lambda/enums/SALUDOS.py
@@ -0,0 +1,7 @@
+SALUDOS = [
+    "¡Hola! ¡Qué gusto tenerte aquí!",
+    "¡Bienvenido de vuelta!",
+    "¡Hola! Me alegra que estés aquí.",
+    "¡Qué bueno verte por aquí!",
+    "¡Hola! Espero que tengas un excelente día."
+]

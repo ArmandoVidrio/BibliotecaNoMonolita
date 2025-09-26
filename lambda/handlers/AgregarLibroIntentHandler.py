03344ccdf1c7645ad30326808e04a363c1be48b5
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2025-09-26 04:57:59 +0000

    updated lambda/handlers/AgregarLibroIntentHandler.py.

--- lambda/handlers/AgregarLibroIntentHandler.py
+++ lambda/handlers/AgregarLibroIntentHandler.py
@@ -4,7 +4,7 @@ import ask_sdk_core.utils as ask_utils
 
 from helpers.database import DatabaseManager
 from helpers.utils import generar_id_unico, get_random_phrase
-from helpers.frases import ALGO_MAS, PREGUNTAS_QUE_HACER
+from enums import ALGO_MAS, PREGUNTAS_QUE_HACER
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
